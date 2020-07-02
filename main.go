9f00b67241ddc29f177d07820d978e2d2f3192ad
Naeem Khan <naeemukhan14@gmail.com>
2020-07-02 20:18:39 +0400

    Added auth system for authentication.

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"github.com/go-macaron/oauth2"
+	"github.com/go-macaron/session"
+	goauth2 "golang.org/x/oauth2"
 	"gopkg.in/macaron.v1"
 	"io"
 	"log"
@@ -62,9 +65,37 @@ func main() {
 	getMenuFromCSV(menuPath)
 
 	m := macaron.Classic()
+	m.Use(session.Sessioner())
+	m.Use(oauth2.Google(
+		&goauth2.Config{
+			ClientID:     "client_id",
+			ClientSecret: "client_secret",
+			Scopes:       []string{"https://www.googleapis.com/auth/drive"},
+			RedirectURL:  "redirect_url",
+		},
+	))
+
+	// Tokens are injected to the handlers
+	m.Get("/", func(tokens oauth2.Tokens) string {
+		if tokens.Expired() {
+			return "not logged in, or the access token is expired"
+		}
+		return "logged in"
+	})
+
+	// Routes that require a logged in user
+	// can be protected with oauth2.LoginRequired handler.
+	// If the user is not authenticated, they will be
+	// redirected to the login path.
+	m.Get("/restrict", oauth2.LoginRequired, func(tokens oauth2.Tokens) string {
+		return tokens.Access()
+	})
+	
 	m.Get("/", func() string {
 		return "Hello, Go!"
 
 	})
+
 	m.Run()
+
 }
